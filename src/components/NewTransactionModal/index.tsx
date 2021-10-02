c05a14caaae15d946f45dbf9a78b9d2d84b5aa41
M4ster3001-Ubuntu <aldocfjunior@gmail.com>
2021-10-02 11:27:50 -0300

    adicionado os botões

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import Modal from 'react-modal';
+import { Container, TransactionTypeContainer } from './styles';
 import closeImg from '../../assets/close.svg';
-import { Container } from './styles';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -34,6 +36,18 @@ export function NewTransactionModal({
 
         <input type="text" placeholder="Título" />
         <input type="number" placeholder="Valor" />
+
+        <TransactionTypeContainer>
+          <button type="button">
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </button>
+          <button type="button">
+            <img src={outcomeImg} alt="Saída" />
+            <span>Saída</span>
+          </button>
+        </TransactionTypeContainer>
+
         <input type="text" placeholder="Categoria" />
         <button type="submit">Cadastrar</button>
       </Container>
